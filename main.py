67d4fb431be2ac18453070c31f12a28e928f8ccc
Clement-Grandgirard <clement.grandgirard@edu.esiee.fr>
2025-11-13 09:26:54 +0000

    Programme exercice palindromme

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 #### Fonction secondaire
+"""Vérifie si une chaîne est un palindrome."""
+import string
 
 
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """Retourne True si p est un palindrome."""
+    table_punctuation = str.maketrans('', '', string.punctuation)
+    table_accent = str.maketrans("àâäçéèêëîïôöùûüÿ", "aaaceeeeiioouuuy")
+    p = p.replace(" ","").lower().translate(table_punctuation).translate(table_accent)
+    if len(p)%2 == 0:
+        for i in range(len(p)//2):
+            if p[i]!=p[len(p)-i-1]:
+                return False
+    else:
+        for i in range(len(p)//2):
+            if p[i]!=p[len(p)-i-1]:
+                return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """Teste la fonction ispalindrome sur plusieurs mots."""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
